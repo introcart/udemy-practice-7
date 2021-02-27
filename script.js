8742a8ebd0a4f0e946e1afaff473a3497e51b777
Steven Carton <spatc@protonmail.com>
2021-02-26 23:21:08 -0500

    finishing up, adding colors for players

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 'use strict';
 
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--');
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+// Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -17,4 +27,22 @@ btnRoll.addEventListener('click', function () {
   console.log('hello');
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
+  console.log(dice);
+  if (dice !== 1) {
+    // add dice to current score
+    currentScore += dice;
+    document.getElementById(
+      `current--${activePlayer}`
+    ).textContent = currentScore;
+  } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+    // switch to next player
+    // score = current
+    // change colors on both
+    //
+  }
 });
