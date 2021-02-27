c8d45caf209e75e21cf5b86497bbf445b953f781
Steven Carton <spatc@protonmail.com>
2021-02-26 22:46:18 -0500

    implementing show dice

--- script.js
+++ script.js
@@ -2,6 +2,19 @@
 
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 score0El.textContent = 0;
 score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log('hello');
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+});
